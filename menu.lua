bc6e6e730d15c134e498075978ad9b7735af99be
npc <142570475+Npcldtt@users.noreply.github.com>
2024-10-25 23:33:06 +0500

    Update menu.lua

--- menu.lua
+++ menu.lua
@@ -1,73 +1,75 @@
-local ScreenGui = game.Players.LocalPlayer.PlayerGui.EXE.TopBar.Frame
+local menuui = {}
 
-local Menu = Instance.new("Frame")
-Menu.Name = "Menu"
-Menu.BackgroundTransparency = 1
-Menu.Position = UDim2.new(0, 0, 0.09451219439506531, 0)
-Menu.BorderColor3 = Color3.fromRGB(0, 0, 0)
-Menu.Size = UDim2.new(0, 62, 0, 297)
-Menu.BorderSizePixel = 0
-Menu.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-Menu.Parent = ScreenGui
+function menuui.Build(player)
+	local ScreenGui = player.PlayerGui.EXE.TopBar.Frame
+	local Menu = Instance.new("Frame")
+	Menu.Name = "Menu"
+	Menu.BackgroundTransparency = 1
+	Menu.Position = UDim2.new(0, 0, 0.09451219439506531, 0)
+	Menu.BorderColor3 = Color3.fromRGB(0, 0, 0)
+	Menu.Size = UDim2.new(0, 62, 0, 297)
+	Menu.BorderSizePixel = 0
+	Menu.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+	Menu.Parent = ScreenGui
 
-local Code = Instance.new("ImageButton")
-Code.Name = "Code"
-Code.BackgroundTransparency = 1
-Code.Position = UDim2.new(0.29032257199287415, 0, 0.20000000298023224, 0)
-Code.BorderColor3 = Color3.fromRGB(0, 0, 0)
-Code.Size = UDim2.new(0, 35, 0, 38)
-Code.BorderSizePixel = 0
-Code.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-Code.Parent = Menu
+	local Code = Instance.new("ImageButton")
+	Code.Name = "Code"
+	Code.BackgroundTransparency = 1
+	Code.Position = UDim2.new(0.29032257199287415, 0, 0.20000000298023224, 0)
+	Code.BorderColor3 = Color3.fromRGB(0, 0, 0)
+	Code.Size = UDim2.new(0, 35, 0, 38)
+	Code.BorderSizePixel = 0
+	Code.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+	Code.Parent = Menu
 
-local Frame = Instance.new("Frame")
-Frame.Visible = false
-Frame.Position = UDim2.new(-0.5142857432365417, 0, 0, 0)
-Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
-Frame.Size = UDim2.new(0, 7, 0, 38)
-Frame.BorderSizePixel = 0
-Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-Frame.Parent = Code
+	local Frame = Instance.new("Frame")
+	Frame.Visible = false
+	Frame.Position = UDim2.new(-0.5142857432365417, 0, 0, 0)
+	Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
+	Frame.Size = UDim2.new(0, 7, 0, 38)
+	Frame.BorderSizePixel = 0
+	Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+	Frame.Parent = Code
 
-local UICorner = Instance.new("UICorner")
-UICorner.Parent = Frame
+	local UICorner = Instance.new("UICorner")
+	UICorner.Parent = Frame
 
-local Frame = Instance.new("Frame")
-Frame.Position = UDim2.new(-0.014285278506577015, 0, 0, 0)
-Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
-Frame.Size = UDim2.new(0, 5, 0, 38)
-Frame.BorderSizePixel = 0
-Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-Frame.Parent = Frame
+	local Frame = Instance.new("Frame")
+	Frame.Position = UDim2.new(-0.014285278506577015, 0, 0, 0)
+	Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
+	Frame.Size = UDim2.new(0, 5, 0, 38)
+	Frame.BorderSizePixel = 0
+	Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+	Frame.Parent = Frame
 
-local Icon = Instance.new("Frame")
-Icon.Name = "Icon"
-Icon.Position = UDim2.new(-0.08571428805589676, 0, 0, 0)
-Icon.BorderColor3 = Color3.fromRGB(0, 0, 0)
-Icon.Size = UDim2.new(0, 38, 0, 38)
-Icon.BorderSizePixel = 0
-Icon.BackgroundColor3 = Color3.fromRGB(148, 148, 148)
-Icon.Parent = Code
+	local Icon = Instance.new("Frame")
+	Icon.Name = "Icon"
+	Icon.Position = UDim2.new(-0.08571428805589676, 0, 0, 0)
+	Icon.BorderColor3 = Color3.fromRGB(0, 0, 0)
+	Icon.Size = UDim2.new(0, 38, 0, 38)
+	Icon.BorderSizePixel = 0
+	Icon.BackgroundColor3 = Color3.fromRGB(148, 148, 148)
+	Icon.Parent = Code
 
-local UICorner = Instance.new("UICorner")
-UICorner.Parent = Icon
+	local UICorner = Instance.new("UICorner")
+	UICorner.Parent = Icon
 
-local TextLabel = Instance.new("TextLabel")
-TextLabel.FontFace = Font.new("rbxasset://fonts/families/GothamSSm.json", Enum.FontWeight.Bold, Enum.FontStyle.Normal)
-TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
-TextLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
-TextLabel.Text = "<>"
-TextLabel.Size = UDim2.new(0, 38, 0, 38)
-TextLabel.BackgroundTransparency = 1
-TextLabel.Position = UDim2.new(0, 1, 0, 0)
-TextLabel.BorderSizePixel = 0
-TextLabel.TextWrapped = true
-TextLabel.TextSize = 26
-TextLabel.BackgroundColor3 = Color3.fromRGB(39, 39, 39)
-TextLabel.Parent = Icon
+	local TextLabel = Instance.new("TextLabel")
+	TextLabel.FontFace = Font.new("rbxasset://fonts/families/GothamSSm.json", Enum.FontWeight.Bold, Enum.FontStyle.Normal)
+	TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+	TextLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
+	TextLabel.Text = "<>"
+	TextLabel.Size = UDim2.new(0, 38, 0, 38)
+	TextLabel.BackgroundTransparency = 1
+	TextLabel.Position = UDim2.new(0, 1, 0, 0)
+	TextLabel.BorderSizePixel = 0
+	TextLabel.TextWrapped = true
+	TextLabel.TextSize = 26
+	TextLabel.BackgroundColor3 = Color3.fromRGB(39, 39, 39)
+	TextLabel.Parent = Icon
 
-local LocalScript = Instance.new("LocalScript")
-LocalScript.Source = "script.Parent.MouseButton1Click:Connect(function()
+	local LocalScript = Instance.new("LocalScript")
+	LocalScript.Source = "script.Parent.MouseButton1Click:Connect(function()
 	script.Parent.Parent.Parent.Code.Visible = true
 	script.Parent.Parent.Parent.Home.Visible = false
 	script.Parent.Parent.Parent.PlayerPreferences.Visible = false
@@ -136,14 +138,14 @@ TextLabel.Parent = Icon
 
 local LocalScript = Instance.new("LocalScript")
 LocalScript.Source = "script.Parent.MouseButton1Click:Connect(function()
-	script.Parent.Parent.Parent.Home.Visible = false
-	script.Parent.Parent.Parent.Code.Visible = false
-	script.Parent.Parent.Parent.Info.Visible = true
-	script.Parent.Parent.Parent.PlayerPreferences.Visible = false
-	script.Parent.Frame.Visible = true
-	script.Parent.Parent.Home.Frame.Visible = false
-	script.Parent.Parent.PlayerPreferences.Frame.Visible = false
-	script.Parent.Parent.Code.Frame.Visible = false
+script.Parent.Parent.Parent.Home.Visible = false
+script.Parent.Parent.Parent.Code.Visible = false
+script.Parent.Parent.Parent.Info.Visible = true
+script.Parent.Parent.Parent.PlayerPreferences.Visible = false
+script.Parent.Frame.Visible = true
+script.Parent.Parent.Home.Frame.Visible = false
+script.Parent.Parent.PlayerPreferences.Frame.Visible = false
+script.Parent.Parent.Code.Frame.Visible = false
 end)"
 LocalScript.Parent = Info
 
@@ -206,14 +208,14 @@ ImageLabel.Parent = Icon
 
 local LocalScript = Instance.new("LocalScript")
 LocalScript.Source = "script.Parent.MouseButton1Click:Connect(function()
-	script.Parent.Parent.Parent.Home.Visible = false
-	script.Parent.Parent.Parent.Code.Visible = false
-	script.Parent.Parent.Parent.Info.Visible = false
-	script.Parent.Parent.Parent.PlayerPreferences.Visible = true
-	script.Parent.Frame.Visible = true
-	script.Parent.Parent.Home.Frame.Visible = false
-	script.Parent.Parent.Info.Frame.Visible = false
-	script.Parent.Parent.Code.Frame.Visible = false
+script.Parent.Parent.Parent.Home.Visible = false
+script.Parent.Parent.Parent.Code.Visible = false
+script.Parent.Parent.Parent.Info.Visible = false
+script.Parent.Parent.Parent.PlayerPreferences.Visible = true
+script.Parent.Frame.Visible = true
+script.Parent.Parent.Home.Frame.Visible = false
+script.Parent.Parent.Info.Frame.Visible = false
+script.Parent.Parent.Code.Frame.Visible = false
 end)"
 LocalScript.Parent = PlayerPreferences
 
@@ -271,14 +273,16 @@ ImageLabel.Parent = Icon
 
 local LocalScript = Instance.new("LocalScript")
 LocalScript.Source = "script.Parent.MouseButton1Click:Connect(function()
-	script.Parent.Parent.Parent.Code.Visible = false
-	script.Parent.Parent.Parent.Home.Visible = true
-	script.Parent.Parent.Parent.PlayerPreferences.Visible = false
-	script.Parent.Parent.Parent.Info.Visible = false
-	script.Parent.Frame.Visible = true
-	script.Parent.Parent.Code.Frame.Visible = false
-	script.Parent.Parent.PlayerPreferences.Frame.Visible = false
-	script.Parent.Parent.Info.Frame.Visible = false
+script.Parent.Parent.Parent.Code.Visible = false
+script.Parent.Parent.Parent.Home.Visible = true
+script.Parent.Parent.Parent.PlayerPreferences.Visible = false
+script.Parent.Parent.Parent.Info.Visible = false
+script.Parent.Frame.Visible = true
+script.Parent.Parent.Code.Frame.Visible = false
+script.Parent.Parent.PlayerPreferences.Frame.Visible = false
+script.Parent.Parent.Info.Frame.Visible = false
 end)"
 LocalScript.Parent = Home
+end
 
+return menuui
